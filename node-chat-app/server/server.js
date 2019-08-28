25863c08b1fd6d4bf4143cb9844603a8de333b69
alireza <alikasmaei588@gmail.com>
2019-08-28 19:00:57 +0430

    add connect and disconnect event

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,14 +1,26 @@
+const socketIo = require('socket.io');
+const http = require('http');
 const path = require('path');
-var publicpath = path.join(__dirname , '../public');
-
 const express = require('express');
+
 const app = express();
+var server = http.createServer(app);
+var io = socketIo(server);
+const publicpath = path.join(__dirname, '../public');
 
 console.log(__dirname + '/../public');
 console.log(publicpath);
 
 app.use(express.static(publicpath));
 
-app.listen((3000), () => {
+io.on('connection', (socket) => {
+    console.log('New user connected.');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected!');
+    })
+})
+
+server.listen((3000), () => {
     console.log('started on port 3000');
 })
