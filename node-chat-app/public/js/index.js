80b59df87d2b049ed31df1bf897522dacf08113a
alireza <alikasmaei588@gmail.com>
2019-08-31 17:33:36 +0430

    add broadcast and announceing in chat app

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -3,24 +3,16 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to the server');
 
-    socket.emit('createEmail', {
-        to:'rezakasm@gmailcom'
-    })
+    // socket.emit('createMessage', {
+    //     to: 'server',
+    //     text:'hi server!!!!'
+    // })
+})
 
-    socket.emit('createMessage', {
-        to: 'client to server',
-        text:'hahaha..'
-    })
+socket.on('newMessage', function (message) {        //get newMessage from server and show in console
+    console.log('new Message : ', message)
 })
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
-})
-
-socket.on('newEmail', function (email) {
-    console.log('new Email',email);
-})
-
-socket.on('newMessage', function (message) {
-    console.log('newMessage',message)
 })
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -16,21 +16,33 @@ app.use(express.static(publicpath));
 io.on('connection', (socket) => {
     console.log('New user connected.');
 
-    socket.emit('newEmail', {
-        from: 'alikasmaei@gmail.com',
-        text:'what the fuck?'
-    });
-    socket.on('createEmail', (newEmail) => {
-        console.log('create Email',newEmail)
-    })
+    // socket.emit('newMessage', {
+    //     from: 'server',
+    //     text: 'hello client',
+    //     createdAt:'123'
+    // })
 
-    socket.emit('newMessage', {
-        from: 'a',
-        text: 't',
-        createdAt:'cr'
-    })
-    socket.on('createMessage', (newMessage) => {
-        console.log('create Message',newMessage)
+    socket.emit('newMessage', 'welcome to chat app :)');    //welcome every new client
+
+    socket.broadcast.emit('newMessage', {                   //announce other connection about new user
+        from: 'admin',
+        text: 'new user joined.'
+    });
+
+    socket.on('createMessage', (Message) => {       //get createMessage from a connection
+        console.log('create Message', Message);
+
+        // io.emit('newMessage', {                  //send to every single connection
+        //     from: Message.from,
+        //     text: Message.text,
+        //     createdAt: new Date().getTime()
+        // });
+
+        socket.broadcast.emit('newMessage', {       //send to "other" connections
+            from: Message.from,
+            text: Message.text,
+            createdAt:new Date().getTime()
+        })
     })
 
     socket.on('disconnect', () => {
